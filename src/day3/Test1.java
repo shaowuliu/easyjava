26a9b66d074d9a61f848ac259f3cf8365205f224
liu <1910850966@qq.com>
2021-01-06 15:38:42 +0800

    这是第第二次的，在commit过程中，可以进行描述

--- src/day3/Test1.java
+++ src/day3/Test1.java
@@ -1,10 +1,17 @@
 package day3;
 
+import java.util.HashMap;
+
 /**
  * @author lsw-shaowu
  * @date 2021/1/6
  */
 public class Test1 {
-
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap=new HashMap<>();
+        hashMap.put("name","lsw");
+        String name=hashMap.get("name");
+        System.out.println(name);
+    }
 }
 
